b0b0f7d973a9ad0f5eac6f8d8041312fc85aa383
jiashuo <3429378257@qq.com>
2026-01-24 14:23:15 +0800

    feat:add 482(密钥格式化)

---
+++ 482.py
@@ -0,0 +1,16 @@
+class Solution:
+    def licenseKeyFormatting(self, s: str, k: int):
+        s = s.upper().replace('-', '')[::-1]
+        res=''
+        for i in range(0,len(s),k):
+            res+=s[i:i+k]+'-'
+        return res[::-1].lstrip('-')
+
+    class Solution:
+        def licenseKeyFormatting(self, s: str, k: int) -> str:
+            s = s.replace('-', '').upper()[::-1]
+            return "-".join((s[i:i+k] for i in range(0,len(s),k))[::-1])
+
+
+s=Solution()
+print(s.licenseKeyFormatting("2-5g-3-J",2))
