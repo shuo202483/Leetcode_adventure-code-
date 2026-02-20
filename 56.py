451b599b171b8bebe87804f28c427f6af29951c1
jiashuo <3429378257@qq.com>
2026-02-20 13:56:45 +0800

    Update 56.py

--- 56.py
+++ 56.py
@@ -5,6 +5,7 @@ class Solution:
 
         for i in range(1,len(intervals)):
             if intervals[i][0]<=res[-1][1]:
+                #对比当前的start和res中的end
                 res[-1][1]=max(res[-1][1],intervals[i][1])
             else:
                 res.append(intervals[i])
