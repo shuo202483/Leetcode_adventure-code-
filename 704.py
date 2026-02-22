ccc82eed81e73c5d1904b141d3c082f774a40b58
shuo <3429378257@qq.com>
2026-02-22 09:07:59 +0800

    use remote version of 704.py

---
+++ 704.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums, target: int) -> int:
+        l, r = 0, len(nums)
+
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid]==target:
+                return mid
+            if nums[mid] > target:
+                r = mid
+            else:
+                l = mid+1
+
+        return -1
+
+
+"""
+闭区间在处理mid是需要+1或-1
+开区间直接获取mid
+"""
